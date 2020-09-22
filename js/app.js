888b758740c57ba46d9f86dedc65d942402a789a
wattana <endnapadad25@gmail.com>
2020-09-22 23:24:31 +0700

    delete

--- js/app.js
+++ js/app.js
@@ -6,17 +6,17 @@ $(function() {
         console.log(page.id);
         if (page.id === 'Home') {
             $('#back').hide();
-            page.querySelector('#spiderman').onclick = function() {
+            page.querySelector('#detail1').onclick = function() {
                 document.querySelector('#myNavigator').pushPage('views/detail.html');
 
             };
-            page.querySelector('#infinitywar').onclick = function() {
+            page.querySelector('#detail2').onclick = function() {
                 document.querySelector('#myNavigator').pushPage('views/detail2.html');
             };
-            page.querySelector('#endgame').onclick = function() {
+            page.querySelector('#detail3').onclick = function() {
                 document.querySelector('#myNavigator').pushPage('views/detail3.html');
             };
-        } else if (page.id === 'spiderman' || page.id === 'infinitywar' || page.id === 'endgame') {
+        } else if (page.id === 'detail1' || page.id === 'detail2' || page.id === 'detail3') {
             $('#back').show();
             document.querySelector('ons-back-button').onclick = function(event) {
                 document.querySelector('#myNavigator').popPage();
