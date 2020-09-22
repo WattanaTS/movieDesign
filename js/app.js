96d9984c7569927c54b8d7b1ad3ff907315d0ed8
wattana <endnapadad25@gmail.com>
2020-09-22 22:30:13 +0700

    update_images

---
+++ js/app.js
@@ -0,0 +1,62 @@
+$(document).ready(function () {
+    var prev = function () {
+      var carousel = document.getElementById('carousel');
+      carousel.prev();
+    };
+  
+    var next = function () {
+      var carousel = document.getElementById('carousel');
+      carousel.next();
+    };
+  
+    ons.ready(function () {
+      var carousel = document.addEventListener('postchange', function (event) {
+        console.log('Changed to ' + event.activeIndex)
+      });
+    });
+  
+    document.addEventListener('init', function (event) {
+      var page = event.target;
+  
+      if (page.id === 'home') {
+  
+        console.log("i");
+  
+        page.querySelector('#movie1').onclick = function () {
+          console.log("i");
+          document.querySelector('#myNavigator').pushPage('view/detail.html');
+        };
+      } if (page.id === 'home') {
+  
+        console.log("ii");
+  
+        page.querySelector('#movie2').onclick = function () {
+          console.log("ii");
+          document.querySelector('#myNavigator').pushPage('view/detail.html');
+        };
+      }
+  
+      if (page.id === 'home') {
+  
+        console.log("iii");
+  
+        page.querySelector('#movie3').onclick = function () {
+          console.log("iii");
+          document.querySelector('#myNavigator').pushPage('view/detail.html');
+        };
+      }
+      if (page.id === 'home') {
+  
+        console.log("iiii");
+  
+        page.querySelector('#movie4').onclick = function () {
+          console.log("iiii");
+          document.querySelector('#myNavigator').pushPage('view/detail.html');
+        };
+      }
+      if (page.id === 'detail') {
+        page.querySelector('ons-toolbar .center').innerHTML = page.data.title;
+      }
+    });
+  });
+  
